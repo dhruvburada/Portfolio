4f06f0b19ac51bbde420f0b59852bb82ead96520
dhruvburada <buradadhruv35@gmail.com>
2023-09-25 16:32:52 +0530

    redo color change changed image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/altumcode-U0tBTn8UR8I-unsplash.jpg";
+import image from "../images/pawel.jpg";
 
 const imageAltText = "leptop in a dark";
 
